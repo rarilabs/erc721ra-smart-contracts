035ee992d87aa92056111baf31f54b06ae0a3dbc
reddqian <will@rarilabs.com>
2022-05-16 15:18:16 +0800

    Update gas-test.js

--- test/gas-test.js
+++ test/gas-test.js
@@ -1,14 +1,20 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Test ERC721RA", function () {
-  it("Test Minting ...", async function () {
+describe("Test Gas", function () {
+  it("Test ERC721RA ...", async function () {
     const RaNft = await ethers.getContractFactory("ERC721RA_NFT");
-    const raNft = await RaNft.deploy(100000000000, true, "{Some_Address}");
+    const raNft = await RaNft.deploy(100000000000);
     await raNft.deployed();
 
-    const mintOneTx = await raNft.mintERC721RA(1);
+    await raNft.mintERC721RA(10);
+  });
 
-    const mintTenTx = await raNft.mintERC721RA(10);
+  it("Test ERC721A ...", async function () {
+    const ANFT = await ethers.getContractFactory("ERC721A_NFT");
+    const aNft = await ANFT.deploy();
+    await aNft.deployed();
+
+    await aNft.mintERC721A(10);
   });
 });
